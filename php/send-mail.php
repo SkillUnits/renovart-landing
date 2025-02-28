767b623539babca87b6cbae1fc9de9066c39271d
Rostyslav Hubariev <r.gubarev1303@gmail.com>
2025-02-28 10:32:46 +0200

    Script change

--- php/send-mail.php
+++ php/send-mail.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require '/home/renofege/public_html/vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
